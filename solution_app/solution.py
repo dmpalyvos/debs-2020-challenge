5e5298453c4068277443d4b6e544e6935e4cc08c
hannahAtaei <hannah>
2019-10-16 11:12:46 +0200

    Dummy solution, GET and print the data

--- solution_app/solution.py
+++ solution_app/solution.py
@@ -1,1 +1,20 @@
+import requests
+import json
+import os
+import sys
 
+if __name__ == "__main__":
+    host = os.getenv('BENCHMARK_SYSTEM_URL')
+    if host is None or '':
+        print('Error reading Server address!')
+
+    # Get the JSON data and print them
+    while(True):
+        response = requests.get("http://" + host +'/data/')
+        if response.status_code == 404:
+            print(response.json())
+            sys.stdout.flush()
+            break
+
+        data = response.json()
+        print("JSON data: " + data)
