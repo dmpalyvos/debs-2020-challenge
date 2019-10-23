fff78cf625f4078c7059d3d8f984ef6994eafcdb
hannahAtaei <hannah>
2019-10-23 17:59:54 +0200

    Add comments to the Solution file.

--- solution_app/solution.py
+++ solution_app/solution.py
@@ -3,24 +3,25 @@ import json
 import os
 import sys
 import time
+import random
 
-def host_url(host, path):
-    return "http://" + host + path
+def host_url(host):
+    return "http://" + host + "/data/"
 
 def get_batch(host):
-    return requests.get(host_url(host, '/data/'))
+    return requests.get(host_url(host))
 
 def post_result(host, payload):
     headers = {'Content-type': 'application/json'}
-    response = requests.post(host_url(host, '/data/'), json = payload, headers=headers)
+    response = requests.post(host_url(host), json = payload, headers=headers)
 
 def random_results():
-    my_dict = {}
-    my_dict[0] = 1
-    my_dict[1] = True
-    my_dict[2] = 3
-
-    return my_dict
+    my_result = {}
+    my_result['ts'] = random.randint(1000, 1000000)
+    my_result['detected'] = bool(random.getrandbits(1))
+    my_result['event_ts'] = random.randint(1000, 1000000)
+    time.sleep(1)
+    return my_result
 
 if __name__ == "__main__":
     host = os.getenv('BENCHMARK_SYSTEM_URL')
@@ -30,8 +31,15 @@ if __name__ == "__main__":
     if host is None or '':
         print('Error reading Server address!')
 
-    # Get the JSON data and print them
+    print('Getting data in batches...') 
+
+    # Here is an script to get the data in batches and give back the results   
+    # Recieved data is in the format of JSON, with attributes {'idx':,'voltage':,'current':}
+    # For each batch, you produce a result with format {'ts':,'detected':,'event_ts':}
     while(True):
+
+        # Making GET request
+        # Each request will fetch new batch
         response = get_batch(host)
         if response.status_code == 404:
             print(response.json())
@@ -39,7 +47,14 @@ if __name__ == "__main__":
             break
 
         data = response.json()
-        print("JSON data: " , data)
 
-        post_result(host, random_results())
-        time.sleep(1)
+        # RUN YOUR DETECTION ALGORITHM HERE
+        # Randomly generated results
+        example_result = random_results()
+        # Substitute the randomly generated results with your implementation.        
+        # Produce one result tuple per batch, in JSON format {'ts':,'detected':,'event_ts':}
+        # After computing the result and making sure it is in the correct format,
+        # submit it via POST request:
+        post_result(host, example_result)
+
+    print('Results are submitted for all the batches successfuly.')
