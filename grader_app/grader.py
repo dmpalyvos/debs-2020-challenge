c6d1bf91c4816447cef0fc135416cd62f9a8e416
Dimitris Palyvos Giannas <palyvos@chalmers.se>
2019-11-01 10:31:05 +0100

    Create tables in grader.py

--- grader_app/grader.py
+++ grader_app/grader.py
@@ -7,6 +7,34 @@ import constants
 
 from resources import Benchmark, Grader
 
+
+# TODO: Rename columns to more meaningful names
+# TODO: Remove redundant timestamp fields
+def createTables():
+    con = sqlite3.connect(constants.DATABASE_NAME)
+    with con:
+        cursor = con.cursor()
+
+        cursor.execute('''CREATE TABLE sent (
+                        batch INTEGER  PRIMARY KEY NOT NULL,
+                        timestamp DATETIME NOT NULL DEFAULT (strftime('%Y-%m-%d %H:%M:%f', 'now', 'localtime'))
+                        )''')
+
+        cursor.execute('''CREATE TABLE results (
+                        batch INTEGER  PRIMARY KEY NOT NULL,
+                        receivedTimestamp DATETIME,
+                        inputTimestamp INTEGER,
+                        detected INTEGER,
+                        eventTimestamp INTEGER
+                        )''')
+
+        cursor.execute('''CREATE TABLE expected (
+                        inputTimestamp INTEGER,
+                        detected INTEGER,
+                        eventTimestamp INTEGER
+                        )''')
+
+
 app = Flask(__name__)
 api = Api(app)
 
@@ -14,8 +42,5 @@ api.add_resource(Benchmark, constants.BENCHMARK_ENDPOINT)
 api.add_resource(Grader, constants.GRADER_ENDPOINT)
 
 if __name__ == '__main__':
-    with sqlite3.connect(constants.DATABASE_NAME) as con:     
-        cursor = con.cursor()
-        cursor.execute('DELETE FROM sent')
-        cursor.execute('DELETE FROM results')
+    createTables()
     app.run(host=constants.SERVER_HOST, port=constants.SERVER_PORT)
