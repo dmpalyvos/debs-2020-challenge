c18e67a369896c76cee03af1b33482561db56bb1
Dimitris Palyvos <palyvos@chalmers.se>
2019-11-08 13:24:50 +0100

    Grader: Reject input files that have a header.

--- grader_app/state.py
+++ grader_app/state.py
@@ -3,13 +3,20 @@ import subprocess
 import pandas as pd
 import sqlite3
 import constants
+import csv
 
 def readInput(inputFile):
     if not os.path.isfile(inputFile):
         print(f"{inputFile} not found. Please put datafiles in the /dataset folder")
         #raise FileNotFoundError()
         exit(1)
-    return pd.read_csv(inputFile, sep=',', names=['idx', 'voltage', 'current'], iterator=True)
+    verifyInputHasNoHeader(inputFile)
+    return pd.read_csv(inputFile, sep=',', names=['idx', 'voltage', 'current'], header=None, iterator=True)
+
+def verifyInputHasNoHeader(inputFile):
+    with open(inputFile) as csvfile:
+        if csv.Sniffer().has_header(csvfile.read(2048)):
+            raise ValueError('The input file should not have a header!')
 
 inputDf = readInput(constants.INPUT_FILE)
 nextSendIndex = 0
